8c74e74aa7f4142d3046d099984956c707161ef3
murik <dennyisgodxd@gmail.com>
2026-01-06 11:10:56 +0100

    attached maincontroller to mainview & added function signatures

--- src/main/java/dk/easv/pmcexam/GUI/Controllers/MainController.java
+++ src/main/java/dk/easv/pmcexam/GUI/Controllers/MainController.java
@@ -1,6 +1,7 @@
 package dk.easv.pmcexam.GUI.Controllers;
 
 // Java imports
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,5 +12,21 @@ public class MainController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private void onBtnClickAddMovie(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnClickAddGenre(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnClickUpdateRating(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnClickDeleteMovie(ActionEvent actionEvent) {
     }
 }
